b3c7dd3057b839cd43eb40f03f35a94f8bba5968
BST <53167130+supportbstteam@users.noreply.github.com>
2023-08-24 13:11:01 +0530

    Update apiService.js

--- src/apiConnection/apiService.js
+++ src/apiConnection/apiService.js
@@ -1,9 +1,12 @@
 // apiService.js
-const API_BASE_URL = 'http://localhost:3001'; // Replace with your API base URL
+const API_BASE_URL = 'http://teamwebdevelopers.com/charge_construct/api'; // Replace with your API base URL
 
 export async function fetchData(endpoint) {
+   const response = await fetch(`${API_BASE_URL}${endpoint}`);
+   return await response.json();
+  /*
   try {
-    const response = await fetch(`${API_BASE_URL}${endpoint}`);
+   
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
@@ -12,4 +15,5 @@ export async function fetchData(endpoint) {
     console.error('Error fetching data:', error);
     throw error;
   }
+  */
 }
