261d53062dc71c228a673801521ff701a58e6ce5
bstteam104 <bstteam104@gmail.com>
2024-01-18 16:27:09 +0530

    18 Jan 2024

--- src/components/Newsroom/ProjectInsights.js
+++ src/components/Newsroom/ProjectInsights.js
@@ -35,7 +35,7 @@ function ProjectInsights(){
 <div className="col-lg-6 col-md-6 col-sm-12 col-12 overlay-posts" data-aos="fade-up" data-aos-easing="linear"
      data-aos-duration="1500" key={index}>
                     <div className="card bg-dark text-white border-0 rounded-0">
-  <img src={`${process.env.imgpath}/blogPost/${item.banner_img}`} alt={item.post_title} className="card-img"/>
+  <img src={`${process.env.imgpath}/project/${item.image}`} alt={item.post_title} className="card-img"/>
   <div className="card-img-overlay">
   <p className="post-published"><span>Errichtung</span> - <span>Ladeinfrastruktur Betreiber</span></p>
     <h4 className="card-title">{item.post_title}</h4>
