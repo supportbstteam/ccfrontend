2aee6b55b2459e38c6a3f20e7c902252512a0c58
bstteam104 <bstteam104@gmail.com>
2024-01-22 10:46:31 +0530

    Jan 22 2024

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
+        API_URL:"https://teamwebdevelopers.com/charge_construct/api",
         BASE_URL: "https://ccfrontend.vercel.app",
         imgpath: 'https://teamwebdevelopers.com/charge_construct/public/images',
       }
